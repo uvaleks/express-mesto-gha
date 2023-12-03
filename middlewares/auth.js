766d54514a68e17e2b7bb0a6d7fca68552ce6f4d
uvaleks <uvaleks@protonmail.com>
2023-12-03 19:45:53 +0300

    hotfix: 14pr review issues

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,20 +4,23 @@ const UnauthorizedError = require('../errors/unauthorized-error');
 const { JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const { cookie } = req.headers;
+  let payload;
 
-  if (!cookie || !cookie.startsWith('token=')) {
-    next(new UnauthorizedError('Неправильные почта или пароль'));
-  } else {
+  try {
+    const { cookie } = req.headers;
+
+    if (!cookie || !cookie.startsWith('token=')) {
+      throw new UnauthorizedError('Неправильные почта или пароль');
+    }
+
     const token = cookie.replace('token=', '');
 
-    jwt.verify(token, JWT_SECRET)
-      .then((decoded) => {
-        req.user = decoded;
-        next();
-      })
-      .catch((err) => {
-        next(err);
-      });
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    next(err);
   }
+
+  req.user = payload;
+
+  return next();
 };
